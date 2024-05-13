49d4335620e3aefc76958c26a4687b68390a9ccc
Eugênio Bandeira <112191399+eugeniobandeira@users.noreply.github.com>
2024-05-13 18:50:02 -0300

    fix: implementação da dependência lombok

--- src/main/java/br/com/fiap/contatos/model/ContatoModel.java
+++ src/main/java/br/com/fiap/contatos/model/ContatoModel.java
@@ -1,12 +1,17 @@
 package br.com.fiap.contatos.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 import java.time.LocalDate;
-import java.util.Objects;
 
 @Entity
 @Table(name = "tab_contatos")
+@Getter
+@Setter
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
 public class ContatoModel {
     @Id
     @GeneratedValue(
@@ -24,48 +29,4 @@ public class ContatoModel {
     @Column(name = "data_nascimento")
     private LocalDate dataNascimento;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public LocalDate getDataNascimento() {
-        return dataNascimento;
-    }
-
-    public void setDataNascimento(LocalDate dataNascimento) {
-        this.dataNascimento = dataNascimento;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ContatoModel that = (ContatoModel) o;
-        return Objects.equals(id, that.id) && Objects.equals(nome, that.nome) && Objects.equals(email, that.email) && Objects.equals(dataNascimento, that.dataNascimento);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, nome, email, dataNascimento);
-    }
 }
